58ec49d835cc22ef330a7dbae5277b08ebd47332
Brendan Maginnis <brendan@sourcery.ai>
2021-08-04 10:49:23 +0100

    Update extract example

--- refactorings/extract_examples.py
+++ refactorings/extract_examples.py
@@ -4,29 +4,6 @@ HORIZONTAL = 1
 VERTICAL = 2
 
 
-class Sliders:
-    def __init__(self, steps):
-        self._steps = steps
-
-    def create_speed_slider(self):
-        speed_slider = Slider(
-            self._steps, from_=1, to=10, orient=HORIZONTAL, label="Speed"
-        )
-        speed_slider.pack()
-        speed_slider.set(DEFAULT_SPEED)
-        speed_slider.configure(background="red")
-        return speed_slider
-
-    def create_force_slider(self):
-        force_slider = Slider(
-            self._steps, from_=1, to=10, orient=HORIZONTAL, label="Force"
-        )
-        force_slider.pack()
-        force_slider.set(DEFAULT_FORCE)
-        force_slider.configure(background="green")
-        return force_slider
-
-
 def create_sliders():
     speed_slider = Slider(5, from_=1, to=10, orient=HORIZONTAL, label="Speed")
     speed_slider.pack()
